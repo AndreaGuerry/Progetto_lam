3fbbd13160f6d5f306ce3e6bf76c9d846a6a5a5f
AndreaGuerry <andrea.p.guerry@gmail.com>
2022-07-26 14:03:48 +0200

    update

--- old code/last data test.py
+++ old code/last data test.py
@@ -1,19 +1,19 @@
 from preparingdata import finaldata
 import json
 
-rawdatafagt = open("data/fearandgreed.json", "r")
+rawdatafagt = open("../data/fearandgreed.json", "r")
 rfagt = rawdatafagt.read()
 jsonfagt = json.loads(rfagt)
 
-rawdatafagtest = open("data/fearandgreedtest.json", "r")
+rawdatafagtest = open("../data/fearandgreedtest.json", "r")
 rfagtest = rawdatafagtest.read()
 jsonfagtest = json.loads(rfagtest)
 
-rawdatabit = open("data/bitcoininfo.json", "r")
+rawdatabit = open("../data/bitcoininfo.json", "r")
 rbit = rawdatabit.read()
 jsonbit = json.loads(rbit)
 
-rawdatabitest = open("data/bitcoininfotest.json", "r")
+rawdatabitest = open("../data/bitcoininfotest.json", "r")
 rbitest = rawdatabitest.read()
 jsonbitest = json.loads(rbitest)
 
